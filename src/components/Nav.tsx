97e3f1c6e203400705250b150b456aa66caa8094
Tucker <trblackw@ncsu.edu>
2019-04-25 08:27:32 -0400

    add Link components

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,30 @@
 import React, { useContext } from 'react';
 import Logo from '../assets/rickmortylogo.png';
 import { Store } from '../Store';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
 	const { state: { favorites } } = useContext(Store);
 	return (
-		<nav className="flex items-center justify-between flex-wrap mb-0 bg-purple-dark sticky p-6">
-			<div className="flex items-center flex-no-shrink text-white mr-6 sticky">
-				<span className="tracking-tight">
-					<img src={Logo} height={130} width={220} alt="" />
+		<nav className='flex items-center justify-between flex-wrap mb-0 bg-purple-dark sticky p-6'>
+			<div className='flex items-center flex-no-shrink text-white mr-6 sticky'>
+            <span className='tracking-tight'>
+               <Link to="/">
+					<img src={Logo} height={130} width={220} alt='' />
+               </Link>
 				</span>
 			</div>
-			<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-				<div className="text-sm lg:flex-grow">
-					<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4" />
-					<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4" />
+			<div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
+				<div className='text-sm lg:flex-grow'>
+					<a href='#responsive-header' className='block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4' />
+					<a href='#responsive-header' className='block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4' />
 				</div>
 				<div>
-					<a
-						href="#"
-						className="inline-block text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-white hover:bg-green mt-4 lg:mt-0"
-					>
+					<Link
+						to='/favorites'
+						className='inline-block text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-white hover:bg-green mt-4 lg:mt-0'>
 						Favorites: {favorites.length}
-					</a>
+					</Link>
 				</div>
 			</div>
 		</nav>
