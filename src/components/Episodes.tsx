9a7e0a1a54f5b6b878338b2f3459ac141747c3a6
Tucker <trblackw@ncsu.edu>
2019-04-27 12:25:30 -0400

    configure backend gql api via amplify

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -1,25 +1,31 @@
-import React, {useContext, useEffect, Fragment, useState} from 'react';
-import {Store, FETCH_DATA, ADD_TO_FAVORITES, REMOVE_FROM_FAVORITES} from '../Store';
-import {IEpisode, IAction} from '../interfaces';
+import React, { useContext, useEffect, Fragment, useState } from 'react';
+import { Store, FETCH_DATA, ADD_TO_FAVORITES, REMOVE_FROM_FAVORITES } from '../Store';
+import { IEpisode, IAction } from '../interfaces';
 
 const Episodes = () => {
     const {
-        state: {episodes, favorites},
+        state: { episodes, favorites },
         dispatch
     } = useContext(Store);
-    const [open, setOpen] = useState<boolean>(false);
+    const [loading, isLoading] = useState(false);
 
     useEffect(() => {
+        isLoading(true);
         fetchEpisodes();
+        isLoading(false);
     }, []);
+
+    useEffect(() => {
+      console.log(favorites)
+    }, [favorites])
 
     const fetchEpisodes = async (): Promise<void> => {
         try {
             const res = await fetch('https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes');
             const {
-                _embedded: {episodes}
+                _embedded: { episodes }
             } = await res.json();
-            return dispatch({type: FETCH_DATA, payload: episodes});
+            return dispatch({ type: FETCH_DATA, payload: episodes });
         } catch (error) {
             console.error(error);
         }
@@ -30,6 +36,10 @@ const Episodes = () => {
             type: favorites.includes(episode) ? REMOVE_FROM_FAVORITES : ADD_TO_FAVORITES,
             payload: episode
         });
+
+    if (loading) {
+        return <div className='m-2 p-2 border shadow bg-indigo-light text-black rounded-md'>Loading...</div>;
+    }
     return (
         <div className='p-3 m-auto bg-indigo-darker text-grey-lightest'>
             {episodes.length > 1 &&
@@ -38,9 +48,7 @@ const Episodes = () => {
                         <div className='m-4 mx-auto p-5 bg-indigo shadow rounded w-2/5 flex justify-between'>
                             <div>
                                 <h3>
-                                    <a
-                                        href={episode.url}
-                                        className='no-underline text-grey-lightest hover:text-grey-light'>
+                                    <a href={episode.url} className='no-underline text-grey-lightest hover:text-grey-light'>
                                         {episode.name}
                                     </a>
                                 </h3>
@@ -49,7 +57,7 @@ const Episodes = () => {
                                     <img
                                         src={episode.image.medium}
                                         alt='episode image'
-                                        style={{objectFit: 'cover', width: '100px', minHeight: '100px'}}
+                                        style={{ objectFit: 'cover', width: '100px', minHeight: '100px' }}
                                     />
                                 </div>
                             </div>
@@ -65,21 +73,14 @@ const Episodes = () => {
                                 </button>
                             </div>
                         </div>
-                        {open && (
-                            <section data-episode={episode.id}>
-                                <p className='font-thin'>{episode.summary}</p>
-                            </section>
-                        )}
                     </Fragment>
                 ))}
         </div>
     );
 };
 
-const addToFavoritesBtn: string =
-    'bg-green-light mx-2 text-sm hover:bg-green text-grey-lightest shadow py-2 px-4 rounded-full';
-const removeFromFavoritesBtn: string =
-    'bg-red-light mx-2 text-sm hover:bg-red text-grey-lightest shadow py-2 px-4 rounded-full';
+const addToFavoritesBtn: string = 'bg-green-light mx-2 text-sm hover:bg-green text-grey-lightest shadow py-2 px-4 rounded-full';
+const removeFromFavoritesBtn: string = 'bg-red-light mx-2 text-sm hover:bg-red text-grey-lightest shadow py-2 px-4 rounded-full';
 
 const imageContainer = {
     width: '100px',
---
+++ src/interfaces.tsx
@@ -0,0 +1,19 @@
+export interface IAction {
+	type: string;
+	payload: any;
+}
+export interface IState {
+	episodes: Array<IEpisode>;
+	favorites: Array<IEpisode>;
+}
+
+export interface IEpisode {
+	id: number;
+	name: string;
+	url: string;
+	summary: string;
+	image: { medium: string, original: string };
+   season: number;
+   airdate: string;
+   airtime: string;
+}
