442869238ae41c753f08823c66cef687e651d741
Tucker <trblackw@ncsu.edu>
2019-04-26 23:20:22 -0400

    remove nav bar

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, Fragment, useState } from 'react';
 import { Store, FETCH_DATA, ADD_TO_FAVORITES, REMOVE_FROM_FAVORITES } from '../Store';
 import { IEpisode, IAction } from '../interfaces';
-import Nav from './Nav';
 
 const Episodes = () => {
 	const { state: { episodes, favorites }, dispatch } = useContext(Store);
@@ -27,14 +26,16 @@ const Episodes = () => {
 			payload: episode
 		});
 	return (
-		<div className="p-3 m-auto bg-indigo-darker text-grey-lightest">
+		<div className='p-3 m-auto bg-indigo-darker text-grey-lightest'>
 			{episodes.length > 1 &&
 				episodes.map((episode: IEpisode) => (
 					<Fragment key={episode.id}>
-						<div className="m-4 mx-auto p-5 bg-indigo shadow rounded w-2/5 flex justify-between">
+						<div className='m-4 mx-auto p-5 bg-indigo shadow rounded w-2/5 flex justify-between'>
 							<div>
 								<h3>
-									<a href={episode.url} className="no-underline text-grey-lightest hover:text-grey-light">
+									<a
+										href={episode.url}
+										className='no-underline text-grey-lightest hover:text-grey-light'>
 										{episode.name}
 									</a>
 								</h3>
@@ -42,27 +43,26 @@ const Episodes = () => {
 								<div style={imageContainer}>
 									<img
 										src={episode.image.medium}
-										alt="episode image"
+										alt='episode image'
 										style={{ objectFit: 'cover', width: '100px', minHeight: '100px' }}
 									/>
 								</div>
 							</div>
 							<br />
 							<div>
-								<button className="bg-orange-light text-sm mx-2 hover:bg-orange text-grey-lightest shadow py-2 px-4 rounded-full">
+								<button className='bg-orange-light text-sm mx-2 hover:bg-orange text-grey-lightest shadow py-2 px-4 rounded-full'>
 									View summary
 								</button>
 								<button
 									className={favorites.includes(episode) ? removeFromFavoritesBtn : addToFavoritesBtn}
-									onClick={() => toggleFavorite(episode)}
-								>
+									onClick={() => toggleFavorite(episode)}>
 									{favorites.includes(episode) ? 'Remove from' : 'Add to'} favorites
 								</button>
 							</div>
 						</div>
 						{open && (
 							<section data-episode={episode.id}>
-								<p className="font-thin">{episode.summary}</p>
+								<p className='font-thin'>{episode.summary}</p>
 							</section>
 						)}
 					</Fragment>
@@ -71,8 +71,10 @@ const Episodes = () => {
 	);
 };
 
-const addToFavoritesBtn: string = 'bg-green-light mx-2 text-sm hover:bg-green text-grey-lightest shadow py-2 px-4 rounded-full';
-const removeFromFavoritesBtn: string = 'bg-red-light mx-2 text-sm hover:bg-red text-grey-lightest shadow py-2 px-4 rounded-full';
+const addToFavoritesBtn: string =
+	'bg-green-light mx-2 text-sm hover:bg-green text-grey-lightest shadow py-2 px-4 rounded-full';
+const removeFromFavoritesBtn: string =
+	'bg-red-light mx-2 text-sm hover:bg-red text-grey-lightest shadow py-2 px-4 rounded-full';
 
 const imageContainer = {
 	width: '100px',
