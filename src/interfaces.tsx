eb807ce537241f70d487979c8736eafa4fd47ce5
Tucker <trblackw@ncsu.edu>
2019-04-28 16:46:15 -0400

    add character and episode interfaces

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,10 +1,19 @@
+export interface IAppState {
+   episodeState: { episodes: IEpisode[]; updateEpisodes: () => void };
+   characterState: { characters: ICharacter[]; updateCharacters: () => void };
+}
+
 export interface IAction {
    type: string;
    payload: any;
 }
-export interface IState {
-   episodes: Array<IEpisode>;
-   favorites: Array<IEpisode>;
+export interface IEpisodeState {
+   episodes: IEpisode[];
+   favorites: IEpisode[];
+}
+
+export interface ICharacterState {
+   characters: ICharacter[];
 }
 
 export interface IEpisode {
@@ -16,4 +25,29 @@ export interface IEpisode {
    season: number;
    airdate: string;
    airtime: string;
+}
+
+export interface ICharacter {
+   episode: string[];
+   gender: string;
+   id: number;
+   image: string;
+   location: { name: string; url: string };
+   name: string;
+   origin: { name: string; url: string };
+   species: string;
+   status: 'Alive' | 'Dead';
+   type?: string;
+   url: string;
+}
+
+export interface IInfo {
+   count: number;
+   next: string;
+   pages: number;
+   prev?: string;
+}
+
+export interface ReducerFunc {
+   (state: ICharacterState | IEpisodeState | any, { type, payload }: IAction): ICharacterState | IEpisodeState;
 }
