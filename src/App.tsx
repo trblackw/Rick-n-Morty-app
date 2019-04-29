c301538dcc77639f3637374c846086e5c9c9329d
Tucker <trblackw@ncsu.edu>
2019-04-29 08:15:35 -0400

    add component/route for characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React, { Fragment, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const Episodes = lazy<React.FunctionComponent<{}>>((): Promise<{
-	default: React.FunctionComponent<{}>;
+   default: React.FunctionComponent<{}>;
 }> => import('./components/Episodes'));
 const Favorites = lazy<React.FunctionComponent<{}>>((): Promise<{
-	default: React.FunctionComponent<{}>;
+   default: React.FunctionComponent<{}>;
 }> => import('./components/Favorites'));
+const Characters = lazy<React.FunctionComponent<{}>>((): Promise<{
+   default: React.FunctionComponent<{}>;
+}> => import('./components/Characters'));
 import Nav from './components/Nav';
 
 const App = () => (
-	<Fragment>
-		<Router>
-			<Nav />
-			<Switch>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Route exact path='/' component={Episodes} />
-					<Route exact path='/favorites' component={Favorites} />
-				</Suspense>
-			</Switch>
-		</Router>
-	</Fragment>
+   <Fragment>
+      <Router>
+         <Nav />
+         <Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+               <Route exact path='/' component={Episodes} />
+               <Route exact path='/favorites' component={Favorites} />
+               <Route exact path='/characters' component={Characters} />
+            </Suspense>
+         </Switch>
+      </Router>
+   </Fragment>
 );
 
 export default App;
