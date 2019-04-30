3c184be5ddac21bdb0259237df3408d891c29de9
Tucker <trblackw@ncsu.edu>
2019-04-30 19:28:23 -0400

    add component designated for characters

---
+++ src/components/Characters.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useContext } from 'react';
+import { CHARACTERS_URL } from '../Store';
+
+const Characters = () => {
+   useEffect(() => {
+      fetchCharacters();
+   }, []);
+
+   const fetchCharacters = async (): Promise<void> => {
+      const res = await fetch(CHARACTERS_URL);
+      const json = await res.json();
+      console.log(json);
+   };
+   return <div>CHARACTERS</div>;
+};
+
+export default Characters;
