2f3e6563b5b960030e924862ab4e51d6cfb59c17
Tucker <trblackw@ncsu.edu>
2019-05-04 12:03:40 -0400

    configure amplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import { StoreProvider } from './Store';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 
-ReactDOM.render(
+Amplify.configure(aws_exports);
+
+render(
    <StoreProvider>
       <App />
    </StoreProvider>,
