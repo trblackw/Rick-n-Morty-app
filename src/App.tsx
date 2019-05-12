66f8a93483f9e206e59279e64bdec4d2e99e439c
Tucker <trblackw@ncsu.edu>
2019-05-12 14:20:42 -0400

    revert lazy loading of episodes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, { Fragment, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-const Episodes = lazy<React.FunctionComponent<{}>>((): Promise<{
-   default: React.FunctionComponent<{}>;
-}> => import('./components/Episodes'));
-const Favorites = lazy<React.FunctionComponent<{}>>((): Promise<{
-   default: React.FunctionComponent<{}>;
+import { BrowserRouter as Router, Switch, Route, RouteComponentProps } from 'react-router-dom';
+import Episodes from './components/Episodes';
+const Favorites = lazy<React.FC<{}>>((): Promise<{
+   default: React.FC<{}>;
 }> => import('./components/Favorites'));
-const Characters = lazy<React.FunctionComponent<{}>>((): Promise<{
-   default: React.FunctionComponent<{}>;
+const Characters = lazy<React.FC<{}>>((): Promise<{
+   default: React.FC<{}>;
 }> => import('./components/Characters'));
 import Episode from './components/Episode';
 import Nav from './components/Nav';
