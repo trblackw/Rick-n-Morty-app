ea62f50779a91cb23344aa839d8968df0c3966f3
Tucker <trblackw@ncsu.edu>
2019-05-12 14:21:04 -0400

    incorporate combinereducers function and modify the way state is exposed to app

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,18 +1,19 @@
-import React, { createContext, useReducer, useEffect, useState } from 'react';
-import { IEpisodeState, IAction, IEpisode, ReducerFunc, ICharacterState, IAppState } from './interfaces';
+import React, { createContext, useReducer } from 'react';
+import { IEpisodeState, IAction, IEpisode, ReducerFunc, ICharacterState } from './interfaces';
 
 //EPISODES
-export const EPISODES_URL: string = 'https://rickandmortyapi.com/api/episode/';
+const EPISODES_URL: string = 'https://rickandmortyapi.com/api/episode/';
 export const FETCH_EPISODES: string = 'FETCH_EPISODES';
 export const ADD_TO_FAVORITES: string = 'ADD_TO_FAVORITES';
 export const REMOVE_FROM_FAVORITES: string = 'REMOVE_FROM_FAVORITES';
 export const CLEAR_FAVORITES: string = 'CLEAR_FAVORITES';
 export const FETCH_SINGLE_EPISODE: string = 'FETCH_SINGLE_EPISODE';
+export const generateEpisodesUrl = (page: number = 1) => `${EPISODES_URL}?page=${page}`;
 
-const episodeReducer: ReducerFunc = (state: IEpisodeState, { type, payload }: IAction): IEpisodeState => {
+const episodeReducer: ReducerFunc = (state: IEpisodeState = initialEpisodeState, { type, payload }: IAction): IEpisodeState => {
    switch (type) {
       case FETCH_EPISODES:
-         return { ...state, episodes: payload };
+         return { ...state, episodes: payload.episodes, info: payload.info };
       case ADD_TO_FAVORITES:
          const uniqueFavorites: IEpisode[] = [...state.favorites, payload].reduce((unique: IEpisode[], episode: IEpisode): IEpisode[] => {
             let episodeIds: number[] = unique.map(({ id }: IEpisode): number => id);
@@ -22,8 +23,8 @@ const episodeReducer: ReducerFunc = (state: IEpisodeState, { type, payload }: IA
          localStorage.setItem('favorites', JSON.stringify(uniqueFavorites));
          return { ...state, favorites: uniqueFavorites };
       case FETCH_SINGLE_EPISODE:
-      localStorage.setItem('selectedEpisode', JSON.stringify(payload));
-         return { ...state, selectedEpisode: payload }
+         localStorage.setItem('selectedEpisode', JSON.stringify(payload));
+         return { ...state, selectedEpisode: payload };
       case REMOVE_FROM_FAVORITES:
          localStorage.clear();
          localStorage.setItem('favorites', JSON.stringify(state.favorites && state.favorites.filter(({ id }) => id !== payload.id)));
@@ -40,7 +41,7 @@ const episodeReducer: ReducerFunc = (state: IEpisodeState, { type, payload }: IA
 export const CHARACTERS_URL: string = 'https://rickandmortyapi.com/api/character/';
 export const FETCH_CHARACTERS: string = 'FETCH_CHARACTERS';
 
-const characterReducer: ReducerFunc = (state: ICharacterState, { type, payload }: IAction): ICharacterState => {
+const characterReducer: ReducerFunc = (state: ICharacterState = initialCharacterState, { type, payload }: IAction): ICharacterState => {
    switch (type) {
       case FETCH_CHARACTERS:
          return { ...state, characters: payload };
@@ -49,8 +50,19 @@ const characterReducer: ReducerFunc = (state: ICharacterState, { type, payload }
    }
 };
 
+const combineReducers = (reducers: { [reducer: string]: ReducerFunc }): ((state: any, action: any) => { state: any }) => {
+   return (state: any = {}, action: IAction) => {
+      return Object.keys(reducers).reduce((nextState: any, key: any) => {
+         // eslint-disable-next-line no-param-reassign
+         nextState[key] = reducers[key](state[key], action);
+         return nextState;
+      }, {});
+   };
+};
+
 const initialEpisodeState: IEpisodeState = {
    episodes: [],
+   info: {},
    favorites: localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites') || '') : []
 };
 
@@ -60,27 +72,13 @@ const initialCharacterState: ICharacterState = {
 export const Store = createContext<ICharacterState | IEpisodeState | any>({ ...initialEpisodeState, ...initialCharacterState });
 
 export const StoreProvider = ({ children }: any): JSX.Element => {
-   const [episodes, updateEpisodes] = useReducer(episodeReducer, initialEpisodeState);
-   const [characters, updateCharacters] = useReducer(characterReducer, initialCharacterState);
-   //todo: address 'any' type
-   const [appState, setAppState] = useState<IAppState | any>({
-      episodeState: { episodes, updateEpisodes },
-      characterState: { characters, updateCharacters }
+   //state key is a required prop on initialState arg passed to useReducer
+   const appReducers = combineReducers({
+      episodeState: episodeReducer,
+      characterState: characterReducer
    });
-   const generateAppState = () =>
-      setAppState({
-         episodeState: { episodes, updateEpisodes },
-         characterState: { characters, updateCharacters }
-      });
-   useEffect(
-      () => {
-         generateAppState();
-      },
-      [episodes, characters]
-   );
+   const [state, dispatch] = useReducer<any>(appReducers, { ...initialEpisodeState, ...initialCharacterState });
+   console.log('TCL: state', state);
 
-   return <Store.Provider value={{ state: appState }}>{children}</Store.Provider>;
+   return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>;
 };
-
-
-// https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes
