3f1284af677640faebab3de5cc9ded1190d0b87d
Tucker <trblackw@ncsu.edu>
2019-05-22 18:54:14 -0400

    rearrange imports to satisfy eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { Fragment, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Episodes from './components/Episodes';
+import Episode from './components/Episode';
+import Nav from './components/Nav';
 const Favorites = lazy<React.FC<{}>>((): Promise<{
    default: React.FC<{}>;
 }> => import('./components/Favorites'));
 const Characters = lazy<React.FC<{}>>((): Promise<{
    default: React.FC<{}>;
 }> => import('./components/Characters'));
-import Episode from './components/Episode';
-import Nav from './components/Nav';
 
 const App = () => (
    <Fragment>
