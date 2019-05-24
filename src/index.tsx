3613b3edad2f1872b02d9da363d5efef0ec4f5c7
Tucker <trblackw@ncsu.edu>
2019-05-24 13:02:54 -0400

    remove amplify boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import { StoreProvider } from './Store';
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-
-Amplify.configure(aws_exports);
 
 render(
    <StoreProvider>
