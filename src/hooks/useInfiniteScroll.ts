0f26f6c912d55d4fde253b099fc058e768c5c591
Tucker <trblackw@ncsu.edu>
2019-05-27 20:28:34 -0400

    add infinite scroll hook

---
+++ src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect, SetStateAction, Dispatch } from 'react';
+
+const useInfiniteScroll = (callback: (anon: () => void) => void): [boolean, Dispatch<SetStateAction<boolean>>] => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    callback(() => {
+      console.log('called back');
+    });
+  }, [isFetching]);
+
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isFetching) return;
+    setIsFetching(true);
+  }
+
+  return [isFetching, setIsFetching];
+};
+
+export default useInfiniteScroll;
