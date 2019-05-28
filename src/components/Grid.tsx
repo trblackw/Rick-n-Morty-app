9679d7b8ee4eeab4be8e096bd82e4f33d468a43f
Tucker <trblackw@ncsu.edu>
2019-05-27 20:28:21 -0400

    add overflow property

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,18 +5,20 @@ interface Props {
     children: ReactNode | JSX.Element;
     minColumnWidth?: number;
     gridGap?: number;
+    overflow?: "auto" | "scroll" | "visible";
 }
 
-const Grid: React.FC<Props> = ({ children, minColumnWidth = 250, gridGap = 0 }): JSX.Element => (
+const Grid: React.FC<Props> = ({ children, minColumnWidth = 250, gridGap = 0, overflow = "visible" }): JSX.Element => (
     <GridContainer minColumnWidth={minColumnWidth} gridGap={gridGap}>
         {children}
     </GridContainer>
 );
 
-const AutoGrid = (minColumnWidth: number = 250, gridGap: number = 0) => ({
+const AutoGrid = (minColumnWidth: number = 250, gridGap: number = 0, overflow = "visible") => ({
     display: 'grid',
     gridTemplateColumns: `repeat(auto-fill, minmax(${minColumnWidth}px, 1fr))`,
     gridGap,
+    overflow
 });
 
 const GridContainer = styled.div(({ minColumnWidth, gridGap }: { minColumnWidth: number; gridGap: number }) => ({
