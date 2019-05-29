7883e7161ce3e4374c52c91cf51886fb1475971a
Tucker <trblackw@ncsu.edu>
2019-05-29 09:37:27 -0400

    define call signature for returned function

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, Dispatch, SetStateAction } from 'react';
 
-export default (initialValue: boolean) => {
+export default (initialValue: boolean): [boolean, Dispatch<SetStateAction<boolean>>] => {
    const [open, setOpen] = useState<boolean>(initialValue);
-   return [open, useCallback((): void => setOpen(status => !status), [status])];
+   return [open, useCallback((): void => setOpen(status => !status), [initialValue])];
 };
