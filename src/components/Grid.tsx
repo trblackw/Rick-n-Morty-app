71940e5e363433da4b8555ce353c9bb2d96c7530
Tucker <trblackw@ncsu.edu>
2019-05-28 23:02:39 -0400

    make use of overflow prop

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,27 +2,29 @@ import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 
 interface Props {
-    children: ReactNode | JSX.Element;
-    minColumnWidth?: number;
-    gridGap?: number;
-    overflow?: "auto" | "scroll" | "visible";
+   children: ReactNode | JSX.Element;
+   minColumnWidth?: number;
+   gridGap?: number;
+   overflow?: 'auto' | 'scroll' | 'visible';
 }
 
-const Grid: React.FC<Props> = ({ children, minColumnWidth = 250, gridGap = 0, overflow = "visible" }): JSX.Element => (
-    <GridContainer minColumnWidth={minColumnWidth} gridGap={gridGap}>
-        {children}
-    </GridContainer>
+const Grid: React.FC<Props> = ({ children, minColumnWidth = 250, gridGap = 0, overflow = 'visible' }): JSX.Element => (
+   <GridContainer minColumnWidth={minColumnWidth} gridGap={gridGap} overflow={overflow}>
+      {children}
+   </GridContainer>
 );
 
-const AutoGrid = (minColumnWidth: number = 250, gridGap: number = 0, overflow = "visible") => ({
-    display: 'grid',
-    gridTemplateColumns: `repeat(auto-fill, minmax(${minColumnWidth}px, 1fr))`,
-    gridGap,
-    overflow
+const AutoGrid = (minColumnWidth: number = 250, gridGap: number = 0, overflow = 'visible') => ({
+   display: 'grid',
+   gridTemplateColumns: `repeat(auto-fill, minmax(${minColumnWidth}px, 1fr))`,
+   gridGap,
+   overflow
 });
 
-const GridContainer = styled.div(({ minColumnWidth, gridGap }: { minColumnWidth: number; gridGap: number }) => ({
-    ...AutoGrid(minColumnWidth, gridGap),
-}));
+const GridContainer = styled.div(
+   ({ minColumnWidth, gridGap, overflow }: { minColumnWidth: number; gridGap: number; overflow: string }) => ({
+      ...AutoGrid(minColumnWidth, gridGap, overflow)
+   })
+);
 
 export default Grid;
