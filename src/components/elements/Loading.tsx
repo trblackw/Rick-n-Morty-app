4fa0d7eebc9d3af1674937004427842dd0bc6431
Tucker <trblackw@ncsu.edu>
2019-05-30 07:53:09 -0400

    add Loading package/animation/component

---
+++ src/components/elements/Loading.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+interface Props {
+   type?: 'blank' | 'balls' | 'bars' | 'bubbles' | 'cubes' | 'cylon' | 'spin' | 'spinningBubbles' | 'spokes' | undefined;
+   height?: string;
+   width?: string;
+   color?: string;
+}
+
+const Loading: React.FC<Props> = ({ type = 'spin', color = '#FF4500' }): JSX.Element => (
+   <div className='mx-auto w-full text-center bg-indigo-600 h-screen flex justify-center p-10'>
+      <ReactLoading type={type} color={color} height={'20%'} width={'20%'} />
+   </div>
+);
+
+export default Loading;
