826bc3e32f3a26c3a215de0d17ca9f2ef8359bae
Tucker <trblackw@ncsu.edu>
2019-06-01 17:24:55 -0400

    enhance search performance

--- src/components/elements/Search.tsx
+++ src/components/elements/Search.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, ChangeEvent, useState, useCallback, Fragment } from 'react';
+import React, { Dispatch, SetStateAction, ChangeEvent, useState, useCallback, Fragment, useEffect } from 'react';
 import { ICharacter } from '../../interfaces';
 
 export interface Filters {
@@ -38,16 +38,26 @@ const Search: React.FC<Props> = ({
       setActiveFilter(activeFilter);
       setFilters(appliedFilter);
    };
-   const applySearch = useCallback(
+
+   useEffect(
       () => {
-         const query: false | RegExp = search !== '' && new RegExp(search, 'gi');
-         //address 'any' typing
-         const results = searchData.filter((character: ICharacter | any) => query && query.test(character[activeFilter]));
-         setLocalSearchResults(results);
-         setSearchMatches(results);
+         applySearch();
       },
       [search]
    );
+
+   const applySearch = useCallback(() => {
+      const query: false | RegExp = search !== '' && new RegExp(search, 'gi');
+      //address 'any' typing
+      const results = searchData.filter((character: ICharacter | any) => query && query.test(character[activeFilter]));
+      setLocalSearchResults(results);
+      setSearchMatches(results);
+   }, [search]);
+
+   const handleFilterClick = (filter: string) => {
+      applyFilter(filter);
+      setSearch('');
+   };
 
    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
       setSearch(e.target.value);
@@ -59,42 +69,37 @@ const Search: React.FC<Props> = ({
    return (
       <section className='bg-indigo-800 h-50 p-8 rounded-md'>
          <div className='container mx-auto py-4'>
-            <input
-               className='w-full h-16 px-3 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg'
-               type='search'
-               value={search}
-               onChange={handleInput}
-               placeholder={`Search by ${activeFilter}`}
-            />
-            {localSearchResults.length > 0 && search !== "" && (
+            <SearchInput activeFilter={activeFilter} search={search} handleInput={handleInput} />
+            {localSearchResults.length > 0 &&
+            search !== '' && (
                <Fragment>
                   <br />
-                  <span className='text-gray-100 text-sm p-3 absolute inset-x-0 ml-16 top-5 h-8'>{localSearchResults.length} results</span>
+                  <span className='text-gray-100 text-sm px-3 pt-12 absolute inset-x-0 ml-16 top-5 h-8'>{localSearchResults.length} results</span>
                </Fragment>
             )}
             <nav className='flex'>
                <button
-                  onClick={() => applyFilter('name')}
+                  onClick={() => handleFilterClick('name')}
                   className={`text-white py-2 px-3 font-medium mr-3 ${filters.name ? activeClass : inactiveClass}`}>
                   Name
                </button>
                <button
-                  onClick={() => applyFilter('status')}
+                  onClick={() => handleFilterClick('status')}
                   className={`text-white py-2 px-3 font-medium mx-3 ${filters.status ? activeClass : inactiveClass}`}>
                   Status
                </button>
                <button
-                  onClick={() => applyFilter('species')}
+                  onClick={() => handleFilterClick('species')}
                   className={`text-white py-2 px-3 font-medium mx-3 ${filters.species ? activeClass : inactiveClass}`}>
                   Species
                </button>
                <button
-                  onClick={() => applyFilter('type')}
+                  onClick={() => handleFilterClick('type')}
                   className={`text-white py-2 px-3 font-medium mx-3 ${filters.type ? activeClass : inactiveClass}`}>
                   Type
                </button>
                <button
-                  onClick={() => applyFilter('gender')}
+                  onClick={() => handleFilterClick('gender')}
                   className={`text-white py-2 px-3 font-medium mx-3 ${filters.gender ? activeClass : inactiveClass}`}>
                   Gender
                </button>
@@ -105,3 +110,54 @@ const Search: React.FC<Props> = ({
 };
 
 export default Search;
+
+const SearchInput: React.FC<{ activeFilter: string; search: string; handleInput: (e: ChangeEvent<HTMLInputElement>) => void }> = ({
+   activeFilter,
+   search,
+   handleInput
+}): JSX.Element => {
+   const inputClasses: string = 'w-full h-16 px-3 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg';
+
+   const renderOptions = (): JSX.Element => {
+      switch (activeFilter) {
+         case 'status':
+            return (
+               <Fragment>
+                  <option value='Alive' />
+                  <option value='Dead' />
+                  <option value="unknown" />
+               </Fragment>
+            );
+         case 'gender':
+            return (
+               <Fragment>
+                  <option value='Male' />
+                  <option value='Female' />
+                  <option value='Genderless' />
+                  <option value='Unknown' />
+               </Fragment>
+            );
+         default:
+            return <option value='' label='Oops' />;
+      }
+   };
+   if (activeFilter === 'status' || activeFilter === 'gender') {
+      return (
+         <Fragment>
+            <input
+               list='characters'
+               className={inputClasses}
+               type='search'
+               value={search}
+               onChange={handleInput}
+               placeholder={`Search by ${activeFilter}`}
+            />
+            <datalist id='characters'>{renderOptions()}</datalist>
+         </Fragment>
+      );
+   } else {
+      return (
+         <input className={inputClasses} type='search' value={search} onChange={handleInput} placeholder={`Search by ${activeFilter}`} />
+      );
+   }
+};
