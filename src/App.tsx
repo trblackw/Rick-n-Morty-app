73b9f61b8096d0632108914c034b6a28065aa60c
Tucker <trblackw@ncsu.edu>
2019-06-07 10:13:20 -0400

    remove landing page in favor of episode list being home view

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Episode from './components/views/Episode';
 import Nav from './components/elements/Nav';
 import Loading from './components/elements/Loading';
-import Landing from './components/views/Landing';
+import Episodes from './components/views/Episodes';
 const Favorites = lazy<React.FC<{}>>((): Promise<{
    default: React.FC<{}>;
 }> => import('./components/views/Favorites'));
 const Characters = lazy<React.FC<{}>>((): Promise<{
    default: React.FC<{}>;
 }> => import('./components/views/Characters'));
-const Episodes = lazy<React.FC<{}>>((): Promise<{
-   default: React.FC<{}>;
-}> => import('./components/views/Episodes'));
 
 const App = () => (
    <Fragment>
@@ -20,8 +17,7 @@ const App = () => (
          <Nav />
          <Switch>
             <Suspense fallback={<Loading />}>
-               <Route exact path='/' component={Landing} />
-               <Route path='/episodes' component={Episodes} />
+               <Route exact path='/' component={Episodes} />
                <Route path='/episode/:id' component={Episode} />
                <Route path='/favorites' component={Favorites} />
                <Route path='/characters' component={Characters} />
